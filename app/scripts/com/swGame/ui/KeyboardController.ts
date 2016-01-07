347d0875d8499f240b326a6e67d33b9b2757c17d
Hadzidy Recilla <hadzidy@grupow.com>
2016-01-07 11:16:19 -0600

    KeyboardController setup

---
+++ app/scripts/com/swGame/ui/KeyboardController.ts
@@ -0,0 +1,41 @@
+/**
+ * Created by had on 1/7/16.
+ */
+module com.swGame.ui {
+
+
+    export class KeyboardController {
+
+        static UP_KEY   : number = 38;
+        static RIGHT_KEY   : number = 39;
+        static LEFT_KEY   : number = 37;
+        static SHOOT_KEY   : number = 32;
+
+        private static KEY_PRESS:Object = {};
+
+        public static initialize(document:JQuery):void {
+
+            KeyboardController.KEY_PRESS[KeyboardController.UP_KEY] = false;
+            KeyboardController.KEY_PRESS[KeyboardController.RIGHT_KEY] = false;
+            KeyboardController.KEY_PRESS[KeyboardController.LEFT_KEY] = false;
+            KeyboardController.KEY_PRESS[KeyboardController.SHOOT_KEY] = false;
+
+            document.keydown((e) => {
+                e.preventDefault();
+                KeyboardController.KEY_PRESS[e.keyCode] = true;
+            });
+
+            document.keyup((e) => {
+                e.preventDefault();
+                KeyboardController.KEY_PRESS[e.keyCode] = false;
+            });
+
+        }
+
+        public static isKeyDown(pressKey:number):boolean {
+            return KeyboardController.KEY_PRESS[pressKey];
+        }
+        
+    }
+
+}
